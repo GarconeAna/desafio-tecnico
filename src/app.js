b67386b4b3eb29963e8fad70803fe5ae6c422a1d
ana carolina garcone <Ana Carolina>
2022-04-05 19:35:30 -0300

    connected to the db

--- src/app.js
+++ src/app.js
@@ -2,6 +2,19 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => console.log("Connected to the database!"));
+
+console.log(process.env.DB_URL);
+
+app.get("/", (req, res) => {
+    res.send("Hello Hello!!")
+})
 
 app.use(express.json());
 app.use(morgan("common"));
@@ -25,10 +38,6 @@ app.use((error, req, res, next) => {
         stack: process.env.NODE_ENV === "production" ? "ok" : error.stack
     });
 });
-
-app.get("/", (req, res) => {
-    res.send("Hello Hello!!")
-})
 
 const PORT = 3333;
 
