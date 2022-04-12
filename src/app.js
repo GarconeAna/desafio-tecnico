1a5751c7993937368dd8d866d1ca3b966ea87b46
ana carolina garcone <Ana Carolina>
2022-04-12 00:16:11 -0300

    arquivo principal concluido

--- src/app.js
+++ src/app.js
@@ -1,47 +1,38 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 const morgan = require("morgan");
 const cors = require("cors");
-const mongoose = require("mongoose");
 require("dotenv").config();
+
+const router = require("./routes");
+const middlewares = require("./middleware");
+
+const app = express();
+
+app.use(morgan("common"));
+app.use(express.json());
+app.use(
+    cors({
+        origin: "http://localhost:3000"
+    })
+);
 
 mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, () => console.log("Connected to the database!"));
 
-console.log(process.env.DB_URL);
+// console.log(process.env.DB_URL);
 
-app.get("/", (req, res) => {
-    res.send("Hello Hello!!")
-})
+app.use(router);
 
-app.use(express.json());
-app.use(morgan("common"));
-app.use(
-    cors({
-        origin: "http://localhost:3000"
-    })
-)
-
-app.use((req, res, next) => {
-    const error = new Error(`Not found - ${req.originalUrl}`);
-    res.status(404);
-    next(error);
-});
-
-app.use((error, req, res, next) => {
-    const statusCode = req.statusCode === 200 ? 500 : res.statusCode;
-    res.statusCode = statusCode;
-    res.json({
-        message: error.message,
-        stack: process.env.NODE_ENV === "production" ? "ok" : error.stack
-    });
-});
+//midlleware recurso não encontrado
+app.use(middlewares.notFound);
+//middleware tratar erro
+app.use(middlewares.errorHandling);
 
 const PORT = 3333;
-
 app.listen(PORT, () => {
-    console.log(`Server running on port: ${PORT}`);
+    console.log(`Server running in: ${PORT}`);
 });
 
